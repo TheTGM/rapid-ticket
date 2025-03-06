4bbf258331af7b91ee9f61bed7b32f95226ee69b
Mateo20021 <mateo@ayte.co>
2025-03-05 21:29:03 -0500

    [FEATURE] Add middlewares for endpoints

--- functions/functions_shows/routes.js
+++ functions/functions_shows/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { StatusCodes } = require('http-status-codes');
+const { showFunctionsShowsMiddleware } = require('./middleware/validationMiddleware');
 const api = express.Router();
 
 
-api.post('/testShow', async (req, res, next) => {
+api.post('/testShow', showFunctionsShowsMiddleware, async (req, res, next) => {
     try {
         return res.status(StatusCodes.OK).json({ message: "ok" });
     } catch (error) {
