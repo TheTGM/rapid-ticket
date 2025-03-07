1962735dfecd0400c613a4a940eefcf0953015f8
Mateo20021 <mateo@ayte.co>
2025-03-07 00:48:54 -0500

    [FEATURE] Add lambda auth

--- functions/reservation/routes.js
+++ functions/reservation/routes.js
@@ -9,8 +9,8 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
 });
 
 // Endpoint existente
