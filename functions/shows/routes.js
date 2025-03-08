dd979f0feaa41d2710f615db5595d128f35c4ba0
Mateo20021 <mateo@ayte.co>
2025-03-07 21:38:34 -0500

    [FEATURE] Add full show logic

--- functions/shows/routes.js
+++ functions/shows/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const showController = require('./controllers/showController');
 
-router.get('/', showController.getAllShows);
+router.get('/getAllShows', showController.getAllShows);
 
 router.get('/:id', showController.getShowById);
 
