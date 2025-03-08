b1f20b036e1fe3325b7770893da0db1dee0701df
Mateo20021 <mateo@ayte.co>
2025-03-07 21:48:35 -0500

    [FEATURE] Add full show logic

--- functions/shows/config/db.js
+++ functions/shows/config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const dbConfig = {
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
